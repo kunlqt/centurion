a874187472d122d4ee3ea212c6a2ac3219615a7e
Vasko Mitanov <vasko.mitanov@toyota.com>
2019-04-16 16:49:22 -0700

    updates to docker

--- test_centuriondb/TestCommon.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-inline const char* IndexNameStoreName = "instore";
-inline const char* StringValueIndexStoreName = "ivstore";
-inline const char* DoubleValueIndexStoreName = "idstore";
-inline const char* BooleanValueIndexStoreName = "ibstore";
-
-
